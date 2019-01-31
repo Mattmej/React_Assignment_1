086982aabe40965043539adc9510221d8f0bb8ab
unknown <mattcmej@gmail.com>
2019-01-31 15:00:58 -0800

    Finish all main requirements for app to function. Now only need to style the output.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import UserOutput from './Components/UserOutput';
+import UserInput from './Components/UserInput';
 
 class App extends Component {
+
+  state = { 
+    // usernameList: [
+    //   "Matt", "Gary"
+    // ]
+
+    username: "Matt"
+  }
+
+  changeState = (event) => { 
+    this.setState({
+      username: event.target.value
+    })
+  }
+
+
+
+
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+    // return (
+    //   <div className="App">
+    //     <header className="App-header">
+    //       <img src={logo} className="App-logo" alt="logo" />
+    //       <p>
+    //         Edit <code>src/App.js</code> and save to reload.
+    //       </p>
+    //       <a
+    //         className="App-link"
+    //         href="https://reactjs.org"
+    //         target="_blank"
+    //         rel="noopener noreferrer"
+    //       >
+    //         Learn React
+    //       </a>
+    //     </header>
+    //   </div>
+    // );
+
+
+    return ( 
+      <div>
+        <UserInput changed={this.changeState} username={this.state.username}/>
+        <UserOutput username={this.state.username}/>
+        <UserOutput username={this.state.username}/>
       </div>
-    );
+    )
+
+
   }
 }
 
---
+++ src/Components/UserOutput.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const userOutput = (props) => { 
+    return (
+        <div>
+            <h1>{props.username}</h1>
+            <p>;oeignlbjnro;gjerofg</p>
+            <p>pw3rhrgtrghilbhntr;bger</p>
+        </div>
+    )
+};
+
+export default userOutput;
